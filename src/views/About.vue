0e5dde7d5ffc9cc7a1328f91cc2987ea98df1175
Steve Doggett <artigat1@gmail.com>
2022-03-16 19:19:56 +0000

    Add time to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>And the time is {{ time }}</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      time: new Date().toLocaleTimeString(),
+    };
+  },
+  mounted() {
+    setInterval(() => {
+      this.time = new Date().toLocaleTimeString();
+    }, 1000);
+  },
+};
+</script>
