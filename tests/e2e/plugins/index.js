ef0546a164b9ff6284c67ca70bf197000be8b65d
Steve Doggett <steve@orbitalwitness.com>
2022-03-16 15:29:55 +0000

    just need to add the build.xml at the end

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -16,6 +16,7 @@ module.exports = (on, config) => {
   // }))
 
   return Object.assign({}, config, {
+    baseUrl: "http://localhost:8080",
     fixturesFolder: "tests/e2e/fixtures",
     integrationFolder: "tests/e2e/specs",
     screenshotsFolder: "tests/e2e/screenshots",
