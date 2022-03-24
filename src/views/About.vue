26b2bfa4d084febadd95ac6ea3441b19db7505d3
Steve Doggett <steve@orbitalwitness.com>
2022-03-24 12:19:27 +0000

    Added the time to the about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>The current time is {{ timestamp.toLocaleString() }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: "About",
+
+  data() {
+    return {
+      timestamp: null,
+    };
+  },
+
+  mounted() {
+    this.startClock();
+  },
+
+  methods: {
+    startClock() {
+      setInterval(() => {
+        this.timestamp = new Date();
+      }, 1000);
+    },
+  },
+};
+</script>
